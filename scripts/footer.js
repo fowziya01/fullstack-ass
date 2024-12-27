7a77aecf4e0460b019e08a2bf18ef5a069a5763a
fowziya01 <fowzijunaid@gmail.com>
2024-12-27 20:58:32 +0530

    header and footer

---
+++ scripts/footer.js
@@ -0,0 +1,8 @@
+const footer=()=>{
+    let card=`<div>
+    <h3>Made by Empoer students<span>&copy;All Rights Reserved 2024</span></h3>
+    </div>`;
+    document.getElementById("footer").innerHTML=card;
+
+};
+footer();
